8fdc54a1c5b42b526957a2b7e95e4d37a9733f8c
Slaynash <hugoflores@hotmail.fr>
2017-03-18 16:52:47 +0100

    Added the number of reseved bytes for commands

--- src/fr/slaynash/communication/rudp/Values.java
+++ src/fr/slaynash/communication/rudp/Values.java
@@ -7,6 +7,7 @@ public class Values {
 	public static final long PACKET_TIMEOUT_TIME_NANOSECONDS = 5000000000L;
 	public static final int VERSION_MAJOR = 1;
 	public static final int VERSION_MINOR = 0;
+	public static final int RESEVED_COMMAND_BYTES = 7;
 	
 	
 	public static class connectionStates{
